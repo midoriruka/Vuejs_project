7e5b668e4e5456c0ff2abf8ca49ca48a2202c64e
midoriruka <sijty@hotmail.com>
2018-10-30 23:19:25 +0800

    tj

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 var app=new Vue({
    el:'#app',
-   data:{
-       name:'A',
-       age:16,
-       sex:'male'
+   methods:{
+       clickBtn:function () {
+           alert('点击了按钮');
+       },
+       mousein:function () {
+           console.log('in');
+       },
+       mouseout:function () {
+           console.log('out');
+       },
+       onSubmit:function () {
+           console.log('submit');
+       },
+       onkeyup:function () {
+           console.log('key pressed');
+       },
+
+
    }
 });
